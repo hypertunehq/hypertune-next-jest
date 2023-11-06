6d317ebfb26d1f450a6c99a40e47dee848d04f37
Miraan Tabrez <miraan.tabrez@gmail.com>
2023-11-06 18:41:57 +0000

    update comment

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,21 +1,30 @@
-import nextJest from 'next/jest.js'
- 
+import nextJest from "next/jest.js";
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
-// There is 
-// https://github.com/ai/nanoid/issues/363#issuecomment-1140906651
+/**
+ * Jest has an issue when resolving dual CJS / ESM modules. Instead of resolving
+ * the CJS version, it resolves the ESM one which uses import syntax which it
+ * then complains about.
+ *
+ * Relevant discussion for nanoid:
+ * https://github.com/ai/nanoid/issues/363#issuecomment-1140906651
+ *
+ * To work around this, we modify transformIgnorePatterns so these modules
+ * are transformed by Jest.
+ */
 const esModules = ["nanoid"];
- 
+
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
- 
-  testEnvironment: 'jest-environment-jsdom',
+
+  testEnvironment: "jest-environment-jsdom",
   transformIgnorePatterns: [`/node_modules/(?!${esModules.join("|")})`],
   moduleNameMapper: {
     ...Object.fromEntries(
@@ -25,7 +34,7 @@ const config = {
       ])
     ),
   },
-}
- 
+};
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
